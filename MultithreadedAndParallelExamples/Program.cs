640f27ab62b27ca43ed31331d1abed8ee00491e6
Luiso <lecc628@gmail.com>
2022-09-07 06:07:48 +0200

    Create example using Thread class
    Defines the ThreadExample_01 console application.
    The first thread which is created inside a process
    is called Main thread. It starts first and ends at last.
    Uses some Thread members: CurrentThread, Name, and Priority.

--- MultithreadedAndParallelExamples/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿
-using System;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
---
+++ MultithreadedAndParallelExamples/ThreadExample_01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadExamples
+{
+    public class ThreadExample_01
+    {
+        private const string NULL_STRING = "null";
+
+        public static void Main()
+        {
+            // The first thread which is created inside a process
+            // is called Main thread. It starts first and ends at last.
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine($"Thread name = {GetThreadName(thread)}\nThread priority = {thread.Priority}");
+        }
+
+        private static string GetThreadName(Thread thread) =>
+            $"{(thread.Name is null ? NULL_STRING : "\"" + thread.Name + "\"")}";
+    }
+}
