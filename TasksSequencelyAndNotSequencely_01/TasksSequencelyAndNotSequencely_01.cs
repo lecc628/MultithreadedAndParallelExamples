c8240ff8545b38043c932b3f8aa3c18486064533
Luiso <lecc628@gmail.com>
2022-09-08 07:20:31 +0200

    Create example using Task class
    Defines the TasksSequencelyAndNotSequencely_01 console application.
    Executes tasks sequencely.
    Uses some Task members: Delay and Run.

---
+++ TasksSequencelyAndNotSequencely_01/TasksSequencelyAndNotSequencely_01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExamples
+{
+    public class TasksSequencelyAndNotSequencely_01
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Executes tasks sequencely:");
+            await ExecuteTasksSequencely();
+
+            Console.WriteLine("\nCompleted!");
+        }
+
+        public static async Task ExecuteTasksSequencely()
+        {
+            await Task.Run(
+                async () =>
+                {
+                    await Task.Delay(3000);
+                    Console.WriteLine("Task 1");
+                }
+            );
+
+            await Task.Run(
+                () =>
+                {
+                    Console.WriteLine("Task 2");
+                }
+            );
+
+            await Task.Run(
+                () =>
+                {
+                    Console.WriteLine("Task 3");
+                }
+            );
+        }
+    }
+}
