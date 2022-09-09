3a0f67ce750bd2e151c2600d544518642ba373cf
Luiso <lecc628@gmail.com>
2022-09-09 07:51:28 +0200

    Create example using IAsyncEnumerable interface
    Defines the IAsyncEnumerableExample_01 console application.
    Enumerates a sync stream using Task<IEnumerable<T>>.

---
+++ IAsyncEnumerableExample_01/IAsyncEnumerableExample_01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerableExamples
+{
+    public class IAsyncEnumerableExample_01
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Using enumerable sync stream:\n");
+            await EnumerateSyncStream();
+
+            Console.WriteLine("\n-----------------------------------------------------------------------\n");
+        }
+
+        public static async Task EnumerateSyncStream()
+        {
+            foreach (var value in await GetValuesAsyncUsingTaskIEnumerable())
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        private static async Task<IEnumerable<int>> GetValuesAsyncUsingTaskIEnumerable()
+        {
+            var list = new List<int>();
+
+            for (var i = 1; i <= 4; ++i)
+            {
+                await Task.Delay(1000);
+                list.Add(i);
+            }
+
+            return list;
+        }
+    }
+}
