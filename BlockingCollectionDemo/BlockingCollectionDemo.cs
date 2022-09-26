b9d441a087aab238e6fa5481a09982add3cb99bd
Luiso <lecc628@gmail.com>
2022-09-26 08:47:38 +0200

    Create example using BlockingCollection class
    Defines the BlockingCollectionDemo console application.
    Creates AddTakeDemo class.
    Uses some BlockingCollection members: Add, Take, and CompleteAdding.

---
+++ BlockingCollectionDemo/BlockingCollectionDemo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace BlockingCollectionExamples
+{
+    public class BlockingCollectionDemo
+    {
+        public static async Task Main()
+        {
+            await AddTakeDemo.BC_AddTakeCompleteAdding();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public class AddTakeDemo
+    {
+        // Demonstrates:
+        //      BlockingCollection<T>.Add()
+        //      BlockingCollection<T>.Take()
+        //      BlockingCollection<T>.CompleteAdding()
+        public static async Task BC_AddTakeCompleteAdding()
+        {
+            using BlockingCollection<int> bc = new();
+
+            // Spin up a Task to populate the BlockingCollection.
+            Task t1 = Task.Run(() =>
+            {
+                bc.Add(1);
+                bc.Add(2);
+                bc.Add(3);
+                bc.CompleteAdding();
+            });
+
+            // Spin up a Task to consume the BlockingCollection.
+            Task t2 = Task.Run(() =>
+            {
+                try
+                {
+                    // Consume consume the BlockingCollection.
+                    while (true)
+                    {
+                        Console.WriteLine(bc.Take());
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    // An InvalidOperationException means that Take() was called on a completed collection.
+                    Console.WriteLine("That's All!");
+                }
+            });
+
+            await Task.WhenAll(t1, t2);
+        }
+    }
+}
