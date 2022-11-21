3fd93eb16fa4f5f41804cc0aef1b47fcdb856b8d
Luiso <lecc628@gmail.com>
2022-11-21 07:37:27 +0100

    Create example using BlockingCollection class
    Creates FromToAnyDemo class.
    Uses a bounded BlockingCollection and some BlockingCollection members:
    TryAddToAny and TryTakeFromAny.

--- BlockingCollectionDemo/BlockingCollectionDemo.cs
+++ BlockingCollectionDemo/BlockingCollectionDemo.cs
@@ -18,6 +18,11 @@ namespace BlockingCollectionExamples
             TryTakeDemo.BC_TryTake();
 
             Console.WriteLine("\n-----------------------------------------------------------------------\n");
+
+            Console.WriteLine($"{nameof(FromToAnyDemo)}:\n");
+            FromToAnyDemo.BC_FromToAny();
+
+            Console.WriteLine("\n-----------------------------------------------------------------------\n");
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
@@ -103,6 +108,44 @@ namespace BlockingCollectionExamples
 
             Console.WriteLine("Sum[0..{0}) = {1}, should be {2}", NUM_ITEMS, outerSum, ((NUM_ITEMS * (NUM_ITEMS - 1)) / 2));
             Console.WriteLine("bc.IsCompleted = {0}, should be true", bc.IsCompleted);
+        }
+    }
+
+    public class FromToAnyDemo
+    {
+        // Demonstrates:
+        //      Bounded BlockingCollection<T>
+        //      BlockingCollection<T>.TryAddToAny()
+        //      BlockingCollection<T>.TryTakeFromAny()
+        public static void BC_FromToAny()
+        {
+            BlockingCollection<int>[] bcs = new BlockingCollection<int>[2];
+            bcs[0] = new BlockingCollection<int>(5);  // Collection bounded to 5 items.
+            bcs[1] = new BlockingCollection<int>(5);  // Collection bounded to 5 items.
+
+            // Should be able to add 10 items w/o blocking.
+            int numFailures = 0;
+            for (var i = 0; i < 10; ++i)
+            {
+                if (BlockingCollection<int>.TryAddToAny(bcs, i) == -1)
+                {
+                    ++numFailures;
+                }
+            }
+            Console.WriteLine("TryAddToAny: {0} failures, should be 0", numFailures);
+
+            // Should be able to retrieve 10 items.
+            int numItems = 0;
+            while (BlockingCollection<int>.TryTakeFromAny(bcs, out _) != -1)
+            {
+                ++numItems;
+            }
+            Console.WriteLine("TryTakeFromAny: retrieved {0} items, should be 10", numItems);
+
+            foreach (var bc in bcs)
+            {
+                bc.Dispose();
+            }
         }
     }
 }
