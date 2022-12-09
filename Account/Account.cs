f9f705d53d85cd6e2107d685f2600a5ba30c5b0d
Luiso <lecc628@gmail.com>
2022-12-09 07:09:19 +0100

    Create example using lock statement
    Defines the AccountTest class and checks the correct behavior
    of the Account class.

--- Account/Account.cs
+++ Account/Account.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace LockExamples
 {
@@ -55,10 +56,51 @@ namespace LockExamples
                 return balance;
             }
         }
+    }
 
-        public static void Main()
+    internal class AccountTest
+    {
+        // If you do not used the lock statement, the output is wrong and prints something like:
+        // Balance before operation is:    1000
+        // Balance after operation is:     1988
+        // But if you use the lock statement (what is the correct way), the output is right and prints:
+        // Balance before operation is:    1000
+        // Balance after operation is:     2000
+        public static async Task Main()
         {
-            Console.WriteLine($"{nameof(Account)} class");
+            Account account = new(1000m);
+            var tasks = new Task[100];
+
+            PrintBalance(account.GetBalance(), "Balance before operation is:");
+
+            for (var i = 0; i < tasks.Length; ++i)
+            {
+                tasks[i] = Task.Run(() => Update(account));
+            }
+
+            await Task.WhenAll(tasks);
+
+            PrintBalance(account.GetBalance(), "Balance after operation is:");
+        }
+
+        public static void PrintBalance(decimal balance, string prefixMessage) =>
+            Console.WriteLine($"{prefixMessage}\t{balance}");
+
+        private static void Update(Account account)
+        {
+            decimal[] amounts = { 0m, 2m, -3m, 6m, -2m, -1m, 8m, -5m, 11m, -6m };
+
+            foreach (var amount in amounts)
+            {
+                if (amount >= 0m)
+                {
+                    account.Credit(amount);
+                }
+                else
+                {
+                    account.Debit(amount);
+                }
+            }
         }
     }
 }
