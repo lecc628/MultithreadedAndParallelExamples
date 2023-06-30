acd1a20066c78a8bae7d7a435fa45103406bc290
Luiso <lecc628@gmail.com>
2023-06-30 08:57:25 +0200

    Create example using Interlocked class
    Creates InterlockedExample_WithTask class.
    This class uses Interlocked class in combination with Task class.

--- InterlockedExample_01/InterlockedExample_01.cs
+++ InterlockedExample_01/InterlockedExample_01.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace InterlockedExamples
 {
@@ -29,24 +29,24 @@ namespace InterlockedExamples
         private static readonly CountdownEvent cde = new(1);
 
         // The example displays output like the following:
-        // Generating 10,000 random numbers with a midpoint value. Please, wait...
-        // Thread_1:
-        //    Random point values: 3,421,799
-        //    Random midpoint values: 3,492 (0.102%)
-        // Thread_0:
-        //    Random point values: 3,313,080
-        //    Random midpoint values: 3,273 (0.099%)
+        // Generating 100,000 random numbers with a midpoint value. Please, wait...
         // Thread_2:
-        //    Random point values: 3,165,538
-        //    Random midpoint values: 3,235 (0.102%)
+        //    Random point values: 32,589,098
+        //    Random midpoint values: 32,432 (0.100%)
+        // Thread_0:
+        //    Random point values: 32,664,437
+        //    Random midpoint values: 32,516 (0.100%)
+        // Thread_1:
+        //    Random point values: 34,873,722
+        //    Random midpoint values: 35,052 (0.101%)
         //
-        // Total random point values: 9,900,417
-        // Total random midpoint values: 10,000 (0.101%)
-        public static void Main()
+        // Total random point values: 100,127,257
+        // Total random midpoint values: 100,000 (0.100%)
+        public static void Main_01()
         {
             try
             {
-                // Do not synchronize midpointsCount because at this point only exits one thread (the main thread).
+                // Do not synchronize access to midpointsCount because at this point only exits one thread (the main thread).
                 Console.WriteLine($"Generating {midpointsCount:N0} random numbers with a midpoint value. Please, wait...");
 
                 Thread thread;
@@ -113,6 +113,94 @@ namespace InterlockedExamples
             Console.WriteLine(s);
 
             cde.Signal();
+        }
+    }
+
+    // The following example is similar to the previous one, except that it uses the Task class
+    // instead of the Thread class to generate random midpoint integers. In this example,
+    // a lambda expression replaces the GenerateNumbers thread procedure, and the call to
+    // the Task.WhenAll method eliminates the need for the CountdownEvent object.
+    public class InterlockedExample_WithTask
+    {
+        private const int LOWER_BOUND = 0;
+        private const int UPPER_BOUND = 1001;
+        private const int MIDPOINT = (UPPER_BOUND - LOWER_BOUND) / 2;
+        private const int TASKS_COUNT = 3;
+
+        private static int totalPoints = 0;
+        private static int totalMidpoints = 0;
+        private static int midpointsCount = 100000;
+
+        private static readonly Random rnd = new();
+        private static readonly object rndLock = new();
+
+        // The example displays output like the following:
+        // Generating 100,000 random numbers with a midpoint value. Please, wait...
+        // Task 10:
+        //    Random point values: 34,865,418
+        //    Random midpoint values: 34,757 (0.100%)
+        // Task 8:
+        //    Random point values: 33,013,420
+        //    Random midpoint values: 33,000 (0.100%)
+        // Task 9:
+        //    Random point values: 32,226,099
+        //    Random midpoint values: 32,243 (0.100%)
+        //
+        //Total random point values: 100,104,937
+        //Total random midpoint values: 100,000 (0.100%)
+        public static async Task Main/*_02*/()
+        {
+            // Do not synchronize access to midpointsCount because at this point only exits one thread (the main thread).
+            Console.WriteLine($"Generating {midpointsCount:N0} random numbers with a midpoint value. Please, wait...");
+
+            Task[] tasks = new Task[TASKS_COUNT];
+
+            for (int i = 0; i < TASKS_COUNT; ++i)
+            {
+                tasks[i] = Task.Run(
+                    () =>
+                    {
+                        int point;
+                        int totalPointsLocal = 0;
+                        int totalMidpointsLocal = 0;
+
+                        do
+                        {
+                            lock (rndLock)
+                            {
+                                point = rnd.Next(LOWER_BOUND, UPPER_BOUND);
+                            }
+
+                            if (point == MIDPOINT)
+                            {
+                                Interlocked.Decrement(ref midpointsCount);
+                                ++totalMidpointsLocal;
+                            }
+
+                            ++totalPointsLocal;
+
+                        } while (Volatile.Read(ref midpointsCount) > 0);
+
+                        Interlocked.Add(ref totalPoints, totalPointsLocal);
+                        Interlocked.Add(ref totalMidpoints, totalMidpointsLocal);
+
+                        string s = string.Format("Task {0}:\n", Task.CurrentId)
+                            + string.Format("   Random point values: {0:N0}\n",
+                                totalPointsLocal)
+                            + string.Format("   Random midpoint values: {0:N0} ({1:P3})",
+                                totalMidpointsLocal, totalMidpointsLocal / ((double)totalPointsLocal));
+                        Console.WriteLine(s);
+                    }
+                );
+            }
+
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine();
+            Console.WriteLine("Total random point values: {0:N0}",
+                totalPoints);
+            Console.WriteLine("Total random midpoint values: {0:N0} ({1:P3})",
+                totalMidpoints, totalMidpoints / ((double)totalPoints));
         }
     }
 }
